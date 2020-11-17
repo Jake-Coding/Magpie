1031cf09e865b90f01f57472a783d884614498dc
Jake Bridge <34487865+Jake-Coding@users.noreply.github.com>
2020-11-17 11:47:32 -0700

    activity5: the dumbest refactoring ive ever seen like why didn't they rework the actual disgusting if statement mess but nooooooo they just refactored the RANDOM INT GENERATOR like jeez man a toddler could write better code than this

--- src/Magpie5.java
+++ src/Magpie5.java
@@ -250,6 +250,8 @@ public class Magpie5 {
 	}
 
 	private String[] randomResponses = { "Interesting, tell me more", "Hmmm.",
-			"Do you really think so?", "You don't say." };
+			"Do you really think so?", "You don't say.", 
+			"This is the product of dumb refactoring.", 
+			"Very cool", "That's pog ngl", "ebic" };
 
 }
