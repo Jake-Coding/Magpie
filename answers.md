9d003c5baa703c7a9f677672f06d60c4a450769e
Jake Bridge <34487865+Jake-Coding@users.noreply.github.com>
2020-11-17 11:44:14 -0700

    activity4

---
+++ answers.md
@@ -0,0 +1,5 @@
+Exploration  
+* What would it mean to build a robot?
+* What would it mean to understand french?
+* What makes you think that I like you?
+* What makes you think that I confuse you?
--- src/Magpie4.java
+++ src/Magpie4.java
@@ -46,9 +46,13 @@ public class Magpie4 {
 		// Responses which require transformations
 		else if (findKeyword(statement, "I want to", 0) >= 0) {
 			response = transformIWantToStatement(statement);
+		} else if (findKeyword(statement, "I want", 0) >= 0) {
+			response = transformIWantStatement(statement);
 		}
 
-		else {
+		else if (findKeyword(statement, "I") >= 0 && findKeyword(statement, "you", findKeyword(statement, "I") + 1) >= 0){
+			response = transformISomethingYouStatement(statement);
+		} else {
 			// Look for a two word (you <something> me)
 			// pattern
 			int psn = findKeyword(statement, "you", 0);
@@ -82,6 +86,30 @@ public class Magpie4 {
 		return "What would it mean to " + restOfStatement + "?";
 	}
 
+	private String transformISomethingYouStatement(String statement) {
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals(".")) {
+			statement = statement.substring(0, statement.length() - 1);
+		}
+
+		int psnOfI = findKeyword(statement, "I");
+		int psnOfYou = findKeyword(statement, "you", psnOfI + 1);
+		String restOfStatement = statement.substring(psnOfI + 1, psnOfYou).trim();
+		return "Why do you " + restOfStatement + " me?";
+	}
+
+	private String transformIWantStatement(String statement) {
+		// Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals(".")) {
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		int psn = findKeyword(statement, "I want", 0);
+		String restOfStatement = statement.substring(psn + 6).trim();
+		return "Would you be really happy if you had " + restOfStatement;
+	}
 	/**
 	 * Take a statement with "you <something> me" and transform it into
 	 * "What makes you think that I <something> you?"
